9d7a889bba396fe385174d9be8fe8cc2942052ce
OlegBychkov <115994498+OlegBychkov@users.noreply.github.com>
2023-01-11 17:08:42 +0400

    new URL

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let CATALOG = [];
 // https://api.jsonserve.com/bNWGAm
 // server/catalog.json
 
-fetch('server/catalog.json')
+fetch('https://gist.githubusercontent.com/OlegBychkov/9da6cb46fbbab98b4fd55270f3c949b3/raw/dc9aa4ed154a086a74db6f1b6eb1fe6a7fd818bc/guitar-shop.json')
     .then(res => res.json())
     .then(body => {
         CATALOG = body; 
